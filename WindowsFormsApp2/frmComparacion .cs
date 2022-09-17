83be7b65a484875a7fb9a63b75c296df87ce034c
BautiMenem <112192354+BautiMenem@users.noreply.github.com>
2022-09-17 19:24:47 -0300

    Add files via upload

--- WindowsFormsApp2/frmComparacion .cs
+++ WindowsFormsApp2/frmComparacion .cs
@@ -19,18 +19,54 @@ namespace WindowsFormsApp2
 
         private void btnComparar_Click(object sender, EventArgs e)
         {
-            if (optMayor.Checked)
+            if (optMayor.Checked == true)
             {
-                if (Convert.ToInt32(txtUno.Text) > (Convert.ToInt32(txtDos.Text)
-
-
-                    MessageBox.Show("El numero 1 SI es mayor");
+               if (Convert.ToInt32(txtUno.Text) > Convert.ToInt32(txtDos.Text))
+               {
+                    MessageBox.Show("El Número uno  SI es mayor");
+               }
                 else
                 {
-                    MessageBox.Show("El numero uno NO es mayor");
+                    MessageBox.Show("El Número uno NO es mayor");
                 }
             }
 
+            if (optMenor.Checked == true)
+            {
+                if (Convert.ToInt32(txtUno.Text) < Convert.ToInt32(txtDos.Text))
+                {
+                    MessageBox.Show("El Número uno SI es menor");
+                }
+                else
+                {
+                    MessageBox.Show("El Número uno NO es menor");
+                }
+            }
+
+            if (optIgual.Checked == true)
+            {
+                if (Convert.ToInt32(txtUno.Text) == Convert.ToInt32(txtDos.Text))
+                {
+                    MessageBox.Show("Los Números SI son iguales");
+                }
+                else
+                {
+                    MessageBox.Show("Los Números NO son iguales");
+                }
+            }
+
+            if (optDesigual.Checked == true)
+            {
+                if (Convert.ToInt32(txtUno.Text) != Convert.ToInt32(txtDos.Text))
+                {
+                    MessageBox.Show("Los Números SI son desiguales");
+                }
+                else
+                {
+                    MessageBox.Show("Los Números NO son desiguales");
+                }
+            }
+        
         }
     }
 }
